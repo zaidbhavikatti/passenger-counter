f0d72145b64c5a02eb1f09a8134a0fc21e1e9707
zaidbhavikatti <98631203+zaidbhavikatti@users.noreply.github.com>
2022-04-19 00:52:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let saveEl=document.getElementById("save-el");
+
+let count=0;
+let countEl=document.getElementById("count-el");
+function increment(){
+      count+=1;
+      countEl .textContent=count;
+}
+
+function save(){
+   
+    
+    let newVar= count +  " - ";
+    saveEl.textContent += newVar;
+    console.log(count)
+    countEl.textContent=0;
+    count=0;
+}
+
+
